5951f401efa006b2cd0341944c37132d3c97d518
Damian Barabonkov <damianb@alum.mit.edu>
2025-12-06 13:10:05 +0100

    chore: Bump version to 0.1.0

--- javelin.el
+++ javelin.el
@@ -6,7 +6,7 @@
 ;; Author: Damian Barabonkov
 ;; Keywords: tools languages
 ;; Homepage: https://github.com/DamianB-BitFlipper/javelin.el
-;; Version: 0.1
+;; Version: 0.1.0
 ;; Package-Requires: ((emacs "28.1") (f "0.20.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
